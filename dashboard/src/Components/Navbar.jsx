025aa5b24f61f98918c35bcf7295118da5ba145a
mdfaizan973 <faizan.md9735@gmail.com>
2023-11-16 15:40:27 +0530

    able to make navbar2

--- dashboard/src/Components/Navbar.jsx
+++ dashboard/src/Components/Navbar.jsx
@@ -3,7 +3,29 @@ export default function Navbar() {
   return (
     <div>
       <div className="dash_nav">
-        <div>Dashboard</div>
+        <div className="flex items-center">
+          <svg
+            className="h-4 w-4 text-gray-700 mr-2"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V9z"
+            />
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M9 22V12h6v10"
+            />
+          </svg>
+          <span className="text-gray-800">Dashboard</span>
+        </div>
 
         <ul className="mt-4 flex sm:mt-0">
           <input
